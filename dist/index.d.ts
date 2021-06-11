e85a1ccaf80b7766eddd173d05a248c507c7ca78
Mojtaba Namazi <mojtaba.namazi.ghamsari@gmail.com>
2021-06-11 16:58:32 +0430

    Add Logger and base class

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,4 @@
+import Hengamify from './src/Hengamify';
 import SampleClass from './src/SampleClass';
+export default Hengamify;
 export { SampleClass };
--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
+import Hengamify from './src/Hengamify'
+import Logger from './src/utils/Logger'
 import SampleClass from './src/SampleClass'
 
+export default Hengamify
+
 export {
+    Logger,
     SampleClass
 }
